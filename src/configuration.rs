9fe54079f2e95e2e2af25b285786bd32d44881bc
Vitor Cavalcanti <1920397+vrcca@users.noreply.github.com>
2025-04-24 09:46:47 +0200

    Reestructure project, adds config

---
+++ src/configuration.rs
@@ -0,0 +1,24 @@
+use config::{Config, ConfigError, File, FileFormat};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+pub struct Settings {
+    pub database: DatabaseSettings,
+    pub application_port: u16,
+}
+
+#[derive(Deserialize)]
+pub struct DatabaseSettings {
+    pub username: String,
+    pub password: String,
+    pub port: u16,
+    pub host: String,
+    pub database_name: String,
+}
+
+pub fn get_configuration() -> Result<Settings, ConfigError> {
+    let settings = Config::builder()
+        .add_source(File::new("configuration.yaml", FileFormat::Yaml))
+        .build()?;
+    settings.try_deserialize::<Settings>()
+}
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -2,11 +2,13 @@
 
 use std::net::TcpListener;
 
+use zero2prod::startup::run;
+
 /// Spin up an instance of our app
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
-    let server = zero2prod::run(listener).expect("Failed to bind address");
+    let server = run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
     format!("http://localhost:{port}")
 }
